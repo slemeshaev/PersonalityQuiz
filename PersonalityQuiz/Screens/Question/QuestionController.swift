7664056292fc55ec5a65d7853d98e523df42c2ff
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-01-29 17:30:20 +0300

    Implemented interface for single answer questions

--- PersonalityQuiz/Screens/Question/QuestionController.swift
+++ PersonalityQuiz/Screens/Question/QuestionController.swift
@@ -1,16 +1,17 @@
 //
-//  WelcomeController.swift
+//  QuestionController.swift
 //  PersonalityQuiz
 //
-//  Created by Stanislav Lemeshaev on 23.01.2022.
+//  Created by Stanislav Lemeshaev on 28.01.2022.
 //  Copyright © 2022 slemeshaev. All rights reserved.
 //
 
 import UIKit
 
-class WelcomeController: UIViewController {
+class QuestionController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .yellow
     }
 }
