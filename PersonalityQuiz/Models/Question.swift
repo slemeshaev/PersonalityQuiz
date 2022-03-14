c5c63ca9e0436afed3d688aecc9710681b5dc993
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-03-14 22:02:47 +0300

    Added question list for our test
    - The question list is added to define animal type for person

--- PersonalityQuiz/Models/Question.swift
+++ PersonalityQuiz/Models/Question.swift
@@ -10,4 +10,25 @@ struct Question {
     let text: String
     let type: ResponseType
     let answers: [Answer]
+    
+    static let list: [Question] = [
+        Question(text: "What kind of food do you prefer?", type: .single, answers: [
+            Answer(text: "Fish", type: .cat),
+            Answer(text: "Steak", type: .dog),
+            Answer(text: "Carrot", type: .rabbit),
+            Answer(text: "Corn", type: .turtle),
+        ]),
+        Question(text: "What do you like to do?", type: .multiply, answers: [
+            Answer(text: "Sleep", type: .cat),
+            Answer(text: "Eat", type: .dog),
+            Answer(text: "Read", type: .rabbit),
+            Answer(text: "Swim", type: .turtle),
+        ]),
+        Question(text: "Do you like car trips?", type: .range, answers: [
+            Answer(text: "Hate", type: .cat),
+            Answer(text: "Don't mind", type: .rabbit),
+            Answer(text: "Like", type: .turtle),
+            Answer(text: "Love", type: .dog),
+        ]),
+    ]
 }
